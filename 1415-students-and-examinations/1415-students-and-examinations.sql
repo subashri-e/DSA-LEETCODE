0d18289fcf28870ec136fdf7bb33171b665b80fc
SUBASHRI E <subashrielango@gmail.com>
2025-09-15 19:25:36 +0530

    Time: 963 ms (44.93%), Space: 0B (100%) - LeetHub

---
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -0,0 +1,8 @@
+select stu.student_id, stu.student_name,sub.subject_name,COUNT(e.subject_name) as attended_exams 
+from students stu 
+cross join subjects sub 
+left join examinations e 
+on stu.student_id=e.student_id and sub.subject_name=e.subject_name
+group by stu.student_id, stu.student_name, sub.subject_name
+order by stu.student_id,sub.subject_name;
+
